530e58a62904c92528e6fc424df570ae5a8e6255
Kyaw-Zin-George <ktz1695@gmail.com>
2024-09-05 10:33:13 +0100

    Initial Commit

---
+++ KyawsUber/KyawsUberApp.swift
@@ -0,0 +1,17 @@
+//
+//  KyawsUberApp.swift
+//  KyawsUber
+//
+//  Created by Kyaw Thant Zin(George) on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct KyawsUberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
