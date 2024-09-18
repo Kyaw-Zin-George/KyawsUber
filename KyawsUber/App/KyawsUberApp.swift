9d663f6071c9e023a1187bc4d797f4cb598571b2
Kyaw-Zin-George <ktz1695@gmail.com>
2024-09-19 00:44:02 +0100

    Initializing the KyawsUber by creating a struct named UberMapViewRepresentable and creating functions and extensions within it.

--- KyawsUber/App/KyawsUberApp.swift
+++ KyawsUber/App/KyawsUberApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct KyawsUberApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
---
+++ KyawsUber/Core/View/UberMapViewRepresentable.swift
@@ -0,0 +1,41 @@
+//
+//  UberMapViewRepresentable.swift
+//  KyawsUber
+//
+//  Created by Kyaw Thant Zin(George) on 9/19/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct UberMapViewRepresentable: UIViewRepresentable{
+    
+    
+    let mapView = MKMapView()
+    
+    func makeUIView(context: Context) -> some UIView {
+        mapView.isRotateEnabled = false
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> MapCoordinator {
+        return MapCoordinator(parent: self)
+    }
+}
+extension UberMapViewRepresentable {
+    class MapCoordinator :NSObject, MKMapViewDelegate{
+        let parent: UberMapViewRepresentable
+        
+        init(parent: UberMapViewRepresentable) {
+            self.parent = parent
+            super.init()
+        }
+    }
+}
